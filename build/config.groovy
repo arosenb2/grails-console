cf2a0c4d41cf61ccc931543255ed90eb53425012
Matt Sheehan <mr.sheehan@gmail.com>
2015-04-09 00:00:06 -0500

    version

--- build/config.groovy
+++
@@ -1,7 +0,0 @@
-
-withConfig(configuration) {
-    inline(phase: 'CONVERSION') { source, context, classNode ->
-        classNode.putNodeMetaData('projectVersion', '0.1-SNAPSHOT')
-        classNode.putNodeMetaData('projectName', 'grails-console')
-    }
-}
